79aa3af2aeac91c72d492a74d1baca9870986df7
suichan-00 <bintangrehan2106@gmail.com>
2026-06-29 23:45:28 +0700

    migration: reset proof_path migration logs

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,8 @@ Route::get('/force-recreate-db-secret-xyz', function() {
         \Illuminate\Support\Facades\DB::table('migrations')->whereIn('migration', [
             '2025_09_25_170900_create_reservations_table',
             '2025_09_25_170914_create_payments_table',
-            '2026_06_21_072420_add_driver_and_delivery_to_reservations_table'
+            '2026_06_21_072420_add_driver_and_delivery_to_reservations_table',
+            '2025_06_13_000000_add_proof_path_to_payments_table'
         ])->delete();
 
         \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
